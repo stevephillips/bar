8e8fda17102041606fe5860866812bbada5a95e9
steve <steve@WEBTEST1>
2019-02-15 17:32:57 +0000

    First build, does nothing, got path to exe and put in test batch

--- Tests/Tests.bat
+++ Tests/Tests.bat
@@ -1,9 +1,11 @@
 :: Use this to batch unit test various options to call the Console app with
 
 :: Rename this full path to location on your machine of the FileData.exe
-SET testExe=C:\DEV\app\DeveloperTestExercise-master\DeveloperTestExercise-master\FileData\bin\Debug\FileData.exe
+SET testExe=C:\DEV\app2\DeveloperTestExercise-master\DeveloperTestExercise-master\FileData\bin\Debug\FileData.exe
 
 :: If the first argument is anyone of –v, --v, /v, --version then return the version of the file (use FileDetails.Version to get the version number, don’t worry about accessing the file or checking if it exists etc.)
+
+
 
 
 
